8ebbf888c66f06b417d9c1066e04770c5d368033
manviny <manol@indinet.es>
2018-02-07 16:09:17 +0100

    common

---
+++ AxiosComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+class AxiosComponent extends Component {
+	// class level property
+	// 1.- initialize a list
+	state = { itemsList: [] };
+
+	componentWillMount() {
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+			// 2.- actualize list
+			.then(response => this.setState({ itemsList: response.data }));
+	}
+	
+	// 3.- key is better to be an id instead of title
+	renderList() {
+		return this.state.itemsList.map( item => 
+			<Text key={item.title}>{item.title}</Text> 
+		);
+	}
+
+	render() {	
+		return (
+			<View>
+				{this.renderList()}
+			</View>	
+		);
+	}
+}
+
+export default AxiosComponent;
---
+++ ClassComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ClassComponent extends Component {
+	render() {	
+		return (
+			<View>
+				<Text>My List!!!</Text>
+			</View>	
+		);
+	}
+}
+
+export default ClassComponent;
