19788c4fd36b1397cf83ce58fd5d513c9f55ad4d
manviny <manol@indinet.es>
2019-04-10 21:52:21 +0200

    Update eltiempo.js

--- ejemplos/eltiempo.js
+++ ejemplos/eltiempo.js
@@ -3,40 +3,36 @@ import { View, Text, ScrollView } from 'react-native';
 import axios from 'axios';
 
 export default class AxiosComponent extends React.Component {
-  // 1.- initialize a list
-  state = { name: "", lon: "" };
 
-  componentWillMount() {
-    axios
-      .get( 
-        'http://api.openweathermap.org/data/2.5/weather?q=Denia,es&APPID=**************************'
-      )
-      // 2.- actualize list, our data is in response.data
-      .then(
-        response => 
-        {
-          this.setState({ lon: response.data.coord.lon });
-          this.setState({ name: response.data.name })
-        }
+	// 1.- initialize a list
+	state = { lugar:"", temperatura:"" };
+
+	componentWillMount() {
+		axios.get('http://api.openweathermap.org/data/2.5/weather?q=Denia,es&APPID=**********************')
+			// 2.- actualize list, our data is in response.data
+			.then(response => {
+        console.log(response.data);
+        this.setState({ lugar: response.data.name });
+        this.setState({ temperatura: response.data.main.temp }); 
+      }
       );
-  }
+	}
+	
+	// 3.- Function to render each element (key is better to be an id instead of title)
+	renderList() {
+      return (
+        <View>
+          <Text>lugar: {this.state.lugar}</Text>
+          <Text>temperatura: {this.state.temperatura}</Text>  
+        </View>
+      );
+	}
 
-  // 3.- Function to render each element (key is better to be an id instead of title)
-  renderList() {
-
-		return(
-      <View>
-        <Text>{this.state.name}</Text>
-        <Text>{this.state.lon}</Text>
-      </View>
-    );
-  }
-
-  render() {
-    return (
-      <View>
-        <Text> {this.renderList()}</Text>
-      </View>
-    );
-  }
+	render() {	
+		return (
+		  <View>
+		    {this.renderList()}
+		  </View>	
+		);
+	}
 }
